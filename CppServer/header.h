a0726337e207742b20a19230b7ac62620cecd518
Terry Zhu <terry.zhu@ericsson.com>
2013-05-14 17:12:13 +0800

    add c++ training code

---
+++ CppServer/header.h
@@ -0,0 +1,26 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <algorithm>
+#include <arpa/inet.h>
+#include "errno.h"
+#include "ErrUtil.h"
+#include "iostream"
+#include <netdb.h>
+#include <netinet/in.h>
+#include "stdio.h"
+#include "string.h"
+#include "sys/socket.h"
+#include "vector"
+
+
+
+
+
+
+
+
+
+
+
+#endif
